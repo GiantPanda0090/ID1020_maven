f9fed7d1bd8a893406b556bc141ec41f7dc8d2bc
Qi Li <liqischool@gmail.com>
2016-11-04 20:46:09 +0100

    interative pascal

---
+++ src/backup/ErrorPascal.java
@@ -0,0 +1,14 @@
+package backup;
+
+/**
+ * Created by AlstonQiLee on 2016-11-04.
+ */
+public abstract class ErrorPascal implements Pascal {
+    void printPascal(int n){
+
+    }
+    int binom(int n, int k){
+
+    }
+
+}

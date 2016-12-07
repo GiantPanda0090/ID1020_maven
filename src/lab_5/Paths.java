941c28ae45a5fee16d4f0c1c56fb63b9be2302f5
Giantpanda0090 <liqischool@gmail.com>
2016-12-07 14:04:51 +0100

    3.1 done

--- src/lab_5/Paths.java
+++ src/lab_5/Paths.java
@@ -6,6 +6,7 @@ package lab_5;
     import edu.princeton.cs.algs4.CC;
     import edu.princeton.cs.algs4.DepthFirstSearch;
     import lab_1.IterativePascal;
+    import se.kth.id1020.Edge;
     import se.kth.id1020.Graph;
     import se.kth.id1020.DataSource;
     import se.kth.id1020.Vertex;
@@ -17,49 +18,58 @@ boolean [] mark;
             Graph g = DataSource.load();
             Paths p= new Paths();
             // work on g
-p.mark = new boolean[g.numberOfVertices()];
-p.dfsVis(g,0);
-            p.isConnect(g, 0);
+            p.mark = new boolean[g.numberOfVertices()];
+
+            p.dfsVis(g,0,false);
+           if( p.isConnect(g)) {
+               System.out.println(" ");
+              /* for(int i =0; i<p.mark.length;i++) {
+                   System.out.print("vertex "+ i+ " "+p.mark[i]+ " , ");
+               }*/
+               System.out.println("The graph itself is fully connected");
+           }else {
+               System.out.println(" ");
+               System.out.println("The graph itself is not fully connected");
+           }
 
         }
-    public void dfsVis(Graph g, int vertex){
+    public void dfsVis(Graph g, int vertex,boolean isSub){
+        System.out.print(g.vertex(vertex)+" --> ");
         count++;
         mark[vertex]=true;
-        int w=0;
-        while(g.adj(v).iterator().hasNext()){
-            if(!mark[w]){
-                dfsVis(g,w);
-            }else{
-                w++;
+        for(Edge w:g.adj(vertex)){
+            //int w =vertex;
+            if(!mark[w.to]){
+                if(!isSub) {
+                    dfsVis(g, w.to, false);
+                }else{
+                    dfsVis(g, w.to, true);
+                }
             }
-        }
-    }
-    public boolean isConnect(Graph g, int v){
-        if (!mark[v]){
-            //dfsVis(g,v);
-            return false;
-        }
-        if(v<mark.length){
-            return true;
-        }else{
-            isConnect(g,v++);
+
+            //System.out.println("||");
         }
 
-        return true;
+        //System.out.println(" ");
     }
-    public void subGraph(Graph g, int v){
-        //count++;
-        boolean[] subm= new boolean[g.numberOfVertices()];
-        subm[v]=true;
-        int w=0;
-        while(g.adj(v).iterator().hasNext()){
-            if(!mark[w]){
-                g.adj(v).iterator().next();
-                dfsVis(g,w);
-            }else{
-                w++;
-            }
-        }
+    public boolean isConnect(Graph g){
+        boolean con=true;
+for(int v =0; v<g.numberOfVertices();v++){
+    if (!mark[v]){
+            System.out.println(" ");
+            System.out.println("Subgraph:");
+        dfsVis(g,v,true);
+        con =false;
+    }else {
+
     }
+
+}
+        return con;
+    }
+    public void shortestP(){
+
+    }
+
     }
 
